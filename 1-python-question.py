447f3a25992a282844fb07c0fb417610997f1f40
botakveggie <83680830+botakveggie@users.noreply.github.com>
2021-10-05 00:37:08 +0800

    finished homework and realised i should be doing small commits

--- 1-python-question.py
+++ 1-python-question.py
@@ -12,24 +12,60 @@
 # Complete everything above in a function called processRequest
 # Your should abstract out function as much as reasonably possible
 
-bannedVisitors = ["Amy", "Grace", "Bruce"]
-memberStatus = {
-    "Ally": True,
-    "David": True,
-    "Brendan": False
-}
+    bannedVisitors = ["Amy", "Grace", "Bruce"]
+    memberStatus = {
+        "Ally": True,
+        "David": True,
+        "Brendan": False
+    }
 
-request = {
-    "applicants": ["Amy", "Ally", "David", "Brendan", "Zoho"]
-}
-
-
-def processRequest(request):
-    # Your code here
-    return
+    request = {
+        "applicants": ["Amy", "Ally", "David", "Brendan", "Zoho"]
+    }
 
 
-print(processRequest(request))
+    def processRequest(request):
+        # function to check banned visitors
+        def check_banned(name):
+            return name in bannedVisitors
+        # function to check member status
+        def check_member(name):
+            if name not in memberStatus:
+                memberStatus[name] = False
+            return memberStatus[name]
+        # function to give price based on membership status
+        def ticketPrice(name):
+            if check_member(name): 
+                return 3.5
+            else: 
+                return 5
+
+        applicant_list = request["applicants"]
+
+        result = {}
+        result["bannedApplicants"] = list(filter(check_banned, applicant_list))
+        result["successfulApplicants"] = []
+        result["totalCost"] = 0
+        result["tickets"] = []
+
+        for applicant in applicant_list:
+            if applicant not in result["bannedApplicants"]:
+                result["successfulApplicants"].append(applicant)
+                memberStat = check_member(applicant)
+                
+                ticket = {
+                    "name": applicant,
+                    "membershipStatus": memberStat,
+                    "price": ticketPrice(applicant)
+                    }
+                result["tickets"].append(ticket)
+                result["totalCost"] += ticketPrice(applicant)
+
+        return result
+
+
+    print(processRequest(request))
+
 
 # {
 #   successfulApplicants:
