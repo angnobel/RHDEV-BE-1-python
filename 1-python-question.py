b2d0ef325b90cdbe0976b746334b4c6f49b79e98
Wen Jun <wjranger@Woos-MacBook-Pro.local>
2021-10-08 14:03:28 +0800

    complete homework

--- 1-python-question.py
+++ 1-python-question.py
@@ -4,11 +4,11 @@
 # Price per member is $3.50
 
 # You are to do the following
-# 1. Identify all banned visitors with a filter call
-# 2. Determine the memberships status of all applicants
-# 3. Calculate the total price for all eligible visitors
-# 4. For each valid visitor, return a corresponding ticket in Dictionary form
-# 5. Return an error via thrown exception if applicants is empty
+# 1. Identify all banned visitors with a filter call (Done)
+# 2. Determine the memberships status of all applicants (Done)
+# 3. Calculate the total price for all eligible visitors (Done)
+# 4. For each valid visitor, return a corresponding ticket in Dictionary form (Done)
+# 5. Return an error via thrown exception if applicants is empty (Done)
 # Complete everything above in a function called processRequest
 # Your should abstract out function as much as reasonably possible
 
@@ -25,11 +25,68 @@ request = {
 
 
 def processRequest(request):
-    # Your code here
-    return
+	def banned(x):
+		return True if x in bannedVisitors else False
 
+	def notbanned(x);
+		return True if x not in bannedVisitors else False
 
-print(processRequest(request))
+	
+	def membership(x):
+		if memberStatus.get((request.get("applicants")[x])) is True:
+			return True
+		else:
+			return False
+
+	        members = []
+        i = 0
+        while (i <= 4):
+            if membership(i) is True:
+                members.append(request.get("applicants")[i])
+            i += 1
+    
+        successfulApplicants = list(filter(notbanned, request.get("applicants")))
+        bannedApplicants = list(filter(banned, request.get("applicants")))
+    
+        partone = {
+            "successfulApplicants": successfulApplicants,
+            "bannedApplicants": bannedApplicants,
+            "totalCost": ((len(members)) * 3.5) + ((len(successfulApplicants) - len(members)) * 5.0)
+        }
+        for key in partone:
+            print(key, ' : ', partone[key])
+        print()
+        
+        print("tickets: [")
+        print()
+        
+        results = {}
+        k = 0    
+        while (k < len(request.get("applicants"))):
+            if ((request.get("applicants"))[k] in members):
+                results["name"] = request.get("applicants")[k]
+                results["membershipStatus"] = True
+                results["price"] = 3.50
+                for key in results:
+                    print(key, ' : ', results[key])
+                print()
+            
+            elif ((request.get("applicants"))[k] not in members and (request.get("applicants"))[k] in successfulApplicants):
+                results["name"] = (request.get("applicants"))[k]
+                results["membershipStatus"] = False
+                results["price"] = 5.00
+                for key in results:
+                    print(key, ' : ', results[key])
+                print()
+            k += 1
+        
+        print("]")
+        
+    except Exception:
+        if (len(list(request.get["applicants"])) == 0):
+            print(' {"error": "No applicants"} ')
+            
+processRequest(request)
 
 # {
 #   successfulApplicants:
---
+++ Readme.md
@@ -0,0 +1,15 @@
+Hi Nobel,
+
+1) I am done with the python code. I am not sure whether you want to mark it
+based on aesthetics too so I tried to make it as close to your requirement
+as possible.
+
+2) I did not use the "Return" keyword in the processRequest(request) function.
+Not sure whether it is wrong but I wanted to create a void function like that in
+C.
+
+3) Because of #2, I am unable to use the print(processRequest(request)) because it
+will print out none on top of the list which is incorrect. Instead, I just called out
+the function immediately.
+
+Do let me know if you have any comments.
