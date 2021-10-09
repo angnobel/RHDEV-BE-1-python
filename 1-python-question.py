e8691d5d1486b727bed9fe78acbb3244d7e66a8d
unknown <e0775349@u.nus.edu>
2021-10-09 20:12:39 +0800

    edited

--- 1-python-question.py
+++ 1-python-question.py
@@ -25,10 +25,48 @@ request = {
 
 
 def processRequest(request):
-    # Your code here
-    return
+  def ban(visitor):
+    return visitor in bannedVisitors
+    
+  def successfulapp(visitor):
+    return visitor not in allresults["bannedApplicants"]
+    
+  def checkmembership(name):
+    if name in memberStatus.keys():
+      return memberStatus[name] #return True
+      
+    else:
+      memberStatus[name] = False
+      return memberStatus[name]
 
+  def checkprice(name):
+    if memberStatus[name] == True:
+      return 3.50
 
+    else:
+      return 5
+  try: 
+    if len(request["applicants"]) == 0:
+      raise ValueError
+
+    allresults = {}
+    allresults["bannedApplicants"] = list(filter(ban,request["applicants"]))
+    allresults["successfulApplicants"] = list(filter(successfulapp, request["applicants"]))
+    allresults["tickets"] = []
+    total = 0 
+
+    for name in allresults["successfulApplicants"]: #create one dictionary for each name
+      eachdict = {"name": name,"membershipStatus": checkmembership(name),"price":checkprice(name)}
+      total += checkprice(name)
+      allresults["tickets"].append(eachdict)
+
+    allresults["totalCost"] = total
+
+    return allresults
+
+  except ValueError:
+    return {"error": "No applicants"}
+    
 print(processRequest(request))
 
 # {
