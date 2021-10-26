b6ae74ab5c68d5a0295e30e37b3fd27dcf976295
Shawn <e0726012@u.nus.edu>
2021-10-27 03:03:12 +0800

    removing redundant code

--- Python Tutorial.py
+++ Python Tutorial.py
@@ -11,62 +11,41 @@ memberStatus = {
   "David": True,
   "Brendan": False
   }
+
 def processRequest(request):
   try:
-
     if request["applicants"] == []:
       raise ValueError
-    Removednames = set(request["applicants"]).intersection(bannedVisitors)
-    Removednames = list(Removednames)
+    
+    Removednames = list(filter(lambda a :a in bannedVisitors, request["applicants"]))
+
     if Removednames:
       for x in Removednames:
-        request["applicants"].remove(x)
-    else:
-      Removednames = "no banned visitors"                          #remove banned visitors from request 
-    successfulApplicants_list = request["applicants"]
-    member_ticket_list = []
-    nonmember_ticket_list = []
-    for name in request["applicants"]:
-      if memberStatus.get(name):                                 
-        ticket = {"name" : name, "membershipStatus": True , "price": 3.50 }
-        member_ticket_list.append(ticket)                          #add member to member list
-      else:
-        ticket = {"name":name,"membershipStatus":False , "price":5 }
-        nonmember_ticket_list.append(ticket)                       #add non member to non member list
-    ticket_list = member_ticket_list + nonmember_ticket_list       #add member and non member list to final list
+        request["applicants"].remove(x)          #remove banned visitors from request
+                     
+    ticket_list = []
     total_price = 0
-    for dict in ticket_list:
-      total_price += dict.get("price")                             #calculate total ticket price
+
+    for x in request["applicants"]:
+      if x not in memberStatus or  memberStatus[x] == False:
+        ticket = {"name" : x, "membershipStatus": False , "price":5  }
+        ticket_list.append(ticket)   
+        total_price += 5
+      
+
+      else:
+        ticket = {"name":x,"membershipStatus": True , "price": 3.50 }
+        ticket_list.append(ticket)
+        total_price += 3.5
+
+
     ticket_dict = {
-                    "successfulApplicants" : successfulApplicants_list, 
                     "bannedApplicatns" : Removednames, 
                     "totalCost": total_price, 
                     "tickets": ticket_list 
                   }
+
     return ticket_dict
+
   except ValueError as e:
     return {"error" : "No applicants"}
-
-
-print(processRequest(request))
-
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
